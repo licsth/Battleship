e2d0edafc9af4f56a3976cf895e4eb53aba119aa
Linda Thelen <lcs.thelen@gmail.com>
2024-01-16 23:01:42 +0100

    legend

--- components/HeatMapLegend.tsx
+++ components/HeatMapLegend.tsx
@@ -4,10 +4,19 @@ import { FunctionComponent } from "react";
 export const HeatMapLegend: FunctionComponent = () => {
   return (
     <div>
+      <div
+        style={{
+          width: 50,
+          height: 25,
+        }}
+        className="text-center text-xs text-white flex items-center justify-center striped"
+      >
+        highest
+      </div>
       {range(200, -10, -10).map((num) => (
         <div
           style={{
-            width: 40,
+            width: 50,
             height: 25,
             background: `hsl(${num}, 90%, 50%)`,
           }}
