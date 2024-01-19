dfedde53b381a0d3cc7570fb0d0d472287842722
Julian <77338144+j-b-19@users.noreply.github.com>
2024-01-19 20:03:50 +0100

    Update Main

--- javaImpl/Main.java
+++ javaImpl/Main.java
@@ -1,30 +1,28 @@
-package javaImpl;
-
-import java.io.*;
+package javaimpl;
 
 public class Main {
-    
+
     public static void main(String[] args) {
         // long start = System.currentTimeMillis();
         // int boardSize = 8;
         // int[] ships = new int[]{0, 3, 0, 1};
         // ComputerPlayer player = new ComputerPlayer(boardSize, ships);
-        
+
         // int[][] tmp = player.getNumberOfCombinationsPerSquare(0L, 0L, 0L, ships);
 
         // System.out.println(Arrays.deepToString(tmp));
-        // System.out.println("Finished in " + (System.currentTimeMillis() - start) + "ms.");
+        // System.out.println("Finished in " + (System.currentTimeMillis() - start) +
+        // "ms.");
 
-        Gamestates gs = new Gamestates(8);
-        try {
-            String fileName = "validStates8x8.bin";
-            long[] accepted = gs.tryAllStates(fileName);
-
-            System.out.println("Accepted " + accepted.length + " states, this would take " + (float)(accepted.length*8/1000000000L) + "GB to store.");
-
-        } catch (IOException e) {
-            // Auto-generated catch block
-            e.printStackTrace();
+        Gamestates gs = new Gamestates(8, true);
+        long missedShots = 0b00001111_11111111_11111011_11111010_11111010_10101010_10101010_10101010L;
+        Game.printBoardState(missedShots, 0, 0, 8);
+        long start = System.currentTimeMillis();
+        long[] states = gs.getValidStates(missedShots, 0, 0);
+        System.out.println(System.currentTimeMillis() - start);
+        for (long s : states) {
+            Game.printBoardState(0, 0, s, 8);
+            System.out.println();
         }
     }
 }
