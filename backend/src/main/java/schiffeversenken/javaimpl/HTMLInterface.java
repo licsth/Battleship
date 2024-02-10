56eda2491744df7ba550a159cbac746e26bd7664
Julian Born <julian.born@stud.tu-darmstadt.de>
2024-02-10 01:35:18 +0100

    Make HTMLInterface actually pass the states to the strategies

--- backend/src/main/java/schiffeversenken/javaimpl/HTMLInterface.java
+++ backend/src/main/java/schiffeversenken/javaimpl/HTMLInterface.java
@@ -4,6 +4,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import schiffeversenken.javaimpl.strategies.*;
@@ -20,11 +25,12 @@ public class HTMLInterface {
     System.out.println(
         "Received start request with strategies: " + strategies.defensiveStrategy + " " + strategies.offensiveStrategy);
 
+    long[] states = readStatesIfNecessary(strategies.offensiveStrategy, strategies.defensiveStrategy);
     DefensiveStrategy defensiveStrategy;
     switch (strategies.defensiveStrategy) {
       case "HideShips":
         // TODO pass states
-        defensiveStrategy = new HideShips(new long[0]);
+        defensiveStrategy = new HideShips(states);
         break;
 
       case "RandomPlacement":
@@ -53,6 +59,32 @@ public class HTMLInterface {
 
     this.computerPlayer = new Player(offensiveStrategy, defensiveStrategy);
 
+  }
+
+  /**
+   * This method determines whether the states have to be read in. If so, it does,
+   * otherwise it returns an empty array.
+   * 
+   * @param defensiveStrategy
+   * @param offensiveStrategy
+   * 
+   * @return The states or an empty array if no strategy requires the states to be
+   *         known.
+   */
+  private long[] readStatesIfNecessary(String offensiveStrategy, String defensiveStrategy) {
+    if (defensiveStrategy == "HideShips") {
+      long[] states = new long[Gamestates.STATES_IN_STANDARD_8x8];
+      try (DataInputStream ds = new DataInputStream(
+          new BufferedInputStream(new FileInputStream(Game.GAME_STATES_FILENAME)))) {
+        for (int i = 0; i < states.length; i++) {
+          states[i] = ds.readLong();
+        }
+      } catch (Exception e) {
+        System.out.println("Error reading states");
+      }
+      return states;
+    }
+    return new long[0];
   }
 
   @PostMapping("/api/guess")
